495c2e2fdbd67b2ceb32140bd0c694246a8a7cc4
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2025-04-02 19:42:39 +0000

    Aula 02/04

--- lista1/exercicios_1.2.py
+++ lista1/exercicios_1.2.py
@@ -25,16 +25,54 @@ pontuacao.adicionar_pontos(50)
 pontuacao.mostrar_pontos()
 
 class PersonagemDois:
-    def __init__(self, vida):
-        self.vida = vida
+    def __init__(self):
+        self.vida = 100
     
     def tomar_dano(self, dano):
-        if dano > 0:
-            self.vida -= dano
-            print(self.vida)
-        if self.vida == 0:
+        self.vida -= dano
+        if dano > 0 and self.vida > 0:
+            print(f"Dano: {dano} Vida restante: {self.vida}")
+        else:
             print("Game Over")
 
-pdois = PersonagemDois(100)
+pdois = PersonagemDois()
 pdois.tomar_dano(10)
+pdois.tomar_dano(5)
+pdois.tomar_dano(10)
+pdois.tomar_dano(10)
+pdois.tomar_dano(65)
 
+class Jogador:
+    def __init__(self):
+        self.energia = 50
+
+    def recuperar_energia(self, quantidade):
+        self.energia += quantidade
+        print(f"Aumentou {quantidade} de energia; Energia = {self.energia}")
+    def usar_energia(self, quantidade):
+        self.energia -= quantidade
+        print(f"Energia usada: {quantidade} Restante: {self.energia}")
+        if self.energia < 0:
+            print("Sem energia suficiente!")
+
+jogador = Jogador()
+jogador.usar_energia(60)
+jogador.recuperar_energia(20)
+
+
+class Inimigo:
+    def __init__(self, nome, vida):
+        self.nome = nome
+        self.vida = vida
+        
+
+    def atacar(self, alvo):
+        print(f"{self.nome} atacou!")
+        alvo.tomar_dano(10)
+
+guerreiro = PersonagemDois()
+inimigo = Inimigo("Morte", 100)
+inimigo.atacar(guerreiro)
+
+
+        
